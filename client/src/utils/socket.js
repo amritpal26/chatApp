4ecab38b19c3e3f0f327124b7c87de01df638193
Amritpal Singh <amritpal.am26@gmail.com>
2019-08-25 20:16:41 -0700

    changed the production url

--- client/src/utils/socket.js
+++ client/src/utils/socket.js
@@ -1,6 +1,6 @@
 import io from 'socket.io-client';
 
-const BASE_URL = process.env.NODE_ENV === 'development' ? 'localhost:3000' : 'https://react-chat-express.herokuapp.com/'
+const BASE_URL = process.env.NODE_ENV === 'development' ? 'localhost:3000' : 'https://chat-application-26.herokuapp.com/'
 const socket = io(BASE_URL);
 
 socket.on('connect', (e) => {
